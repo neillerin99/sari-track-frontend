df14f33e13d53073c22bc2faa01b6a2415077bd4
neil99 <lerin113@gmail.com>
2025-11-17 14:15:39 +0800

    feat: header responsive design
    -added shadcn sheet component to handle responsive design
    -added hamburger menu button for small screens

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,7 +1,15 @@
 import { useNavigate } from "@tanstack/react-router";
 import { Button } from "../ui/button";
 import ThemeToggle from "../utils/ThemeToggle";
-
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 export default function Header() {
   const navigate = useNavigate();
   return (
@@ -11,27 +19,45 @@ export default function Header() {
       </span>
       <div className="ml-auto flex items-center gap-2">
         <ThemeToggle />
-        <Button
-          className="cursor-pointer"
-          variant={"ghost"}
-          onClick={() =>
-            navigate({
-              to: "/about",
-            })
-          }
-        >
-          Login
-        </Button>
-        <Button
-          className="cursor-pointer"
-          onClick={() =>
-            navigate({
-              to: "/about",
-            })
-          }
-        >
-          Get Started
-        </Button>
+        <div className="sm:flex hidden">
+          <Button
+            className="cursor-pointer"
+            variant={"ghost"}
+            onClick={() =>
+              navigate({
+                to: "/about",
+              })
+            }
+          >
+            Login
+          </Button>
+          <Button
+            className="cursor-pointer"
+            onClick={() =>
+              navigate({
+                to: "/about",
+              })
+            }
+          >
+            Get Started
+          </Button>
+        </div>
+        <div className="sm:hidden block">
+          <Sheet>
+            <SheetTrigger>
+              <Menu className="cursor-pointer" />
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </header>
   );
