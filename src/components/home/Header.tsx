63e5fa69090b6a977037e760a828ea74deb28542
neil99 <lerin113@gmail.com>
2025-11-17 15:28:46 +0800

    feat: partial max width implementation

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -13,50 +13,52 @@ import {
 export default function Header() {
   const navigate = useNavigate();
   return (
-    <header className="h-14 bg-background flex items-center justify-between p-4">
-      <span className="text-2xl font-bold bg-linear-to-r from-blue-600 to-cyan-600 dark:from-blue-400 dark:to-cyan-400 bg-clip-text text-transparent">
-        SariTrack
-      </span>
-      <div className="ml-auto flex items-center gap-2">
-        <ThemeToggle />
-        <div className="sm:flex hidden">
-          <Button
-            className="cursor-pointer"
-            variant={"ghost"}
-            onClick={() =>
-              navigate({
-                to: "/about",
-              })
-            }
-          >
-            Login
-          </Button>
-          <Button
-            className="cursor-pointer"
-            onClick={() =>
-              navigate({
-                to: "/about",
-              })
-            }
-          >
-            Get Started
-          </Button>
-        </div>
-        <div className="sm:hidden block">
-          <Sheet>
-            <SheetTrigger>
-              <Menu className="cursor-pointer" />
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
+    <header className="h-14 bg-background flex items-center justify-center p-4 w-full">
+      <div className="w-full max-w-7xl flex items-center">
+        <span className="text-2xl font-bold bg-linear-to-r from-blue-600 to-cyan-600 dark:from-blue-400 dark:to-cyan-400 bg-clip-text text-transparent">
+          SariTrack
+        </span>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <div className="sm:flex hidden">
+            <Button
+              className="cursor-pointer"
+              variant={"ghost"}
+              onClick={() =>
+                navigate({
+                  to: "/about",
+                })
+              }
+            >
+              Login
+            </Button>
+            <Button
+              className="cursor-pointer"
+              onClick={() =>
+                navigate({
+                  to: "/about",
+                })
+              }
+            >
+              Get Started
+            </Button>
+          </div>
+          <div className="sm:hidden block">
+            <Sheet>
+              <SheetTrigger>
+                <Menu className="cursor-pointer" />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Are you absolutely sure?</SheetTitle>
+                  <SheetDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </div>
         </div>
       </div>
     </header>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute("/")({
 
 function App() {
   return (
-    <div className="flex flex-col bg-red-500 min-h-full">
+    <div className="flex flex-col bg-red-500 min-h-full items-center">
       <Header />
       <button className="bg-primary text-primary-foreground hover:bg-primary/90">
         Click Me
