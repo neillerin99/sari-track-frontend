6a29dcab5ac31ee2e6b25b1dc833cbc7648675b3
neillerin99 <lerin113@gmail.com>
2025-12-05 20:51:42 +0800

    feat: benefits section UI
    -added benefits section based from design
    -mobile responsive is now also working

--- src/features/home/components/Benefits.tsx
+++ src/features/home/components/Benefits.tsx
@@ -1,7 +1,47 @@
+import Wrapper from "@/components/common/Wrapper";
+import { benefits } from "../utils/benefits";
+import BenefitsRow from "./BenefitsRow";
+import { testimonials } from "../utils/testimonial";
+import type { BenefitTestimonial } from "../../../models/Testimonial";
+import BenefitsTestimonialRow from "./BenefitsTestimonialRow";
+
 export default function Benefits() {
   return (
-    <div className="min-h-56 bg-linear-to-br from-blue-600 to-cyan-600 dark:from-blue-900 dark:to-cyan-900 text-white transition-colors duration-300">
-      Hello
+    <div className="bg-linear-to-br from-blue-600 to-cyan-600 dark:from-blue-900 dark:to-cyan-900 text-white transition-colors duration-300 min-h-fit py-20 px-4">
+      <Wrapper className="flex lg:flex-row flex-col  items-center justify-start sm:py-0 py-5 lg:gap-7 md:gap-15 gap-14 h-full">
+        <BenefitHighlights />
+        <BenefitTestimonials />
+      </Wrapper>
+    </div>
+  );
+}
+
+function BenefitHighlights() {
+  return (
+    <div className="h-full flex-1 flex flex-col gap-6">
+      <h2 className="sm:text-4xl text-3xl font-bold sm:text-start text-center">
+        Why Store Owners Love Us
+      </h2>
+      <p className="sm:text-xl text-lg text-blue-100 dark:text-cyan-100 mb-6">
+        Join hundreds of sari-sari store owners who have transformed their
+        business with our system
+      </p>
+      {benefits.map((benefit: string) => (
+        <BenefitsRow text={benefit} />
+      ))}
+    </div>
+  );
+}
+
+function BenefitTestimonials() {
+  return (
+    <div className="bg-white/10 dark:bg-black/20 backdrop-blur-lg min-h-64 lg:min-h-[400px] flex-1 w-full rounded-xl p-8 flex flex-col justify-between gap-10">
+      {testimonials.map((testimonial: BenefitTestimonial) => (
+        <BenefitsTestimonialRow
+          storeName={testimonial.storeName}
+          testimonial={testimonial.testimonial}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/features/home/utils/benefits.tsx
@@ -0,0 +1,8 @@
+export const benefits: Array<string> = [
+  "Easy to use interface",
+  "Real-time inventory tracking",
+  "Multiple payment methods",
+  "Customer credit management",
+  "Batch & expiration tracking",
+  "Detailed sales reports",
+];
