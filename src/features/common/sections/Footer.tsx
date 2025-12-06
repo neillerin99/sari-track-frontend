cbbe5503aa41963dcb9f5776a00f216c7fd34a36
neillerin99 <lerin113@gmail.com>
2025-12-06 22:29:08 +0800

    feat: footer section final UI

--- src/features/common/sections/Footer.tsx
+++ src/features/common/sections/Footer.tsx
@@ -1,9 +1,29 @@
 import Wrapper from "@/components/common/Wrapper";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "@tanstack/react-router";
 
 export default function Footer() {
+  const navigate = useNavigate();
   return (
-    <div className="min-h-40 bg-gray-900 dark:bg-black text-white py-12 transition-colors duration-300 w-full">
-      <Wrapper className="flex justify-between">hello</Wrapper>
-    </div>
+    <footer className="min-h-40 bg-gray-900 dark:bg-black text-white transition-colors duration-300 w-full flex items-center">
+      <Wrapper className="flex flex-col">
+        <div className="flex justify-between">
+          <Button
+            className="text-2xl font-bold bg-linear-to-r from-(--text-gradient-from) to-(--text-gradient-to) dark:from-(--text-gradient-from) dark:to-(--text-gradient-to) bg-clip-text text-transparent cursor-pointer"
+            onClick={() =>
+              navigate({
+                to: "/",
+              })
+            }
+          >
+            SariTrack
+          </Button>
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} Neil Edriane Lerin. All rights
+            reserved.
+          </p>
+        </div>
+      </Wrapper>
+    </footer>
   );
 }
